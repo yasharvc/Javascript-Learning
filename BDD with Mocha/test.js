4dce82385ce324f5b605c99192edf71b013f8c41
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-02-18 23:29:53 +0330

    Test with a loop inside assertion

--- BDD with Mocha/test.js
+++ BDD with Mocha/test.js
@@ -8,4 +8,15 @@ describe("pow", function() {
     assert.equal(pow(3, 4), 81);
   });
 
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} in the power 3 is ${expected}`, function() {
+      assert.equal(pow(x, 3), expected);
+    });
+  }
+
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+
 });
